0e1d296d488cdaa41211fc430fe562c54d89b728
Ultrainstictmasteryoda <128071340+dababiyoda@users.noreply.github.com>
2025-07-22 12:34:40 -0700

    Create __init__.py

---
+++ src/workflows/__init__.py
@@ -0,0 +1,4 @@
+"""Workflows package for the Wealth Machine project."""
+from .workflow_engine import start_workflow, wealth_machine_flow  # noqa: F401
+
+__all__ = ["start_workflow", "wealth_machine_flow"]
